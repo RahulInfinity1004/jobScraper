0c08aa4d7528ac94b4be0f3d4f8422c20c6eb85a
manzil-infinity180 <rahulvs2809@gmail.com>
2024-12-30 23:09:59 +0530

    feat: lets scrape 1600 companies data

--- src/arc-companies/allCompaniesDetails.ts
+++ src/arc-companies/allCompaniesDetails.ts
@@ -307,7 +307,7 @@ async function scrapeYC(headless: boolean) {
     // await coreLogic(page,url,allCurrentJobs);
 
     // not included 95
-    for (let i=0;i <600;i++){
+    for (let i=0;i <1800;i++){
         console.log("Scraping NO : ", i);
         await coreLogic(page,data.data[i].companyLink, allCurrentJobs);
     }
